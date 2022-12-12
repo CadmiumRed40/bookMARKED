456fb52b35f012bb34e16ec0300f1fb7c354194a
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-12-11 20:56:25 -0500

    @desc server.js: sucessfully connected to database.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//@desc Ensuring we open the app in developement mode
 if(process.env.NODE_ENV !=='production') {
     require('dotenv').config()
 }
@@ -5,26 +6,28 @@ if(process.env.NODE_ENV !=='production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-
+const mongoose = require('mongoose')
 const indexRouter = require('./routes/index')
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') 
 app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
-const mongoose = require('mongoose')
+//@desc Connecting to database.
 mongoose.set('strictQuery', false)
 mongoose.connect(process.env.DATABASE_URL, { 
     useNewUrlParser:true,
-    useUnifiedTopology:true,
-    family: 4,
+    /*useUnifiedTopology:true,
+    family: 4,*/
     })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
+//@desc Routers
 app.use('/', indexRouter)
 
+//@desc Listening Port
 app.listen(process.env.PORT || 3000)
