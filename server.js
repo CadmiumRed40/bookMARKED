59089c8a3271b1f3615d0e8b4685263e06f2c327
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-12-20 21:53:26 -0500

    created 'partials/errormessage', added functionality for adding ,searching through authors in /routes/authors.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ if(process.env.NODE_ENV !=='production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+
 const indexRouter = require('./routes/index')
 const authorRouter = require('./routes/authors')
 
@@ -15,6 +17,7 @@ app.set('views',__dirname + '/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit:'10mb', extended: false} ))
 
 //@desc Connecting to database.
 mongoose.set('strictQuery', false)
