e27ab95f16c10f8414e60c322bdcb5605d8f2e40
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-12-27 01:22:17 -0500

    fixed bug on line 40 of models/book to pull up the proper cover image by assigning this to the proper value

--- models/book.js
+++ models/book.js
@@ -37,7 +37,7 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function(){
-    if(this.name != null) {
+    if(this.coverImageName != null) {
         return path.join('/', coverImageBasePath, this.coverImageName)
     }
 })
