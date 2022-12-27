276d844b78d445f55dd4f9b924f688bcc2232cac
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-12-26 21:03:47 -0500

    error creating new book.

--- routes/books.js
+++ routes/books.js
@@ -3,13 +3,13 @@ const router = express.Router()
 const multer = require('multer')
 const path = require('path')
 const Book = require('../models/book')
+const Author = require('../models/author')
 const uploadPath = path.join('public', Book.coverImageBasePath)
 const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gifs']
-const Author = require('../models/author')
 const upload = multer({
    dest: uploadPath,
    fileFilter: (req, file, callback) => {
-      callback(null, )
+      callback(null, imageMimeTypes.includes(file.mimetype))
    }
 })
 
@@ -20,16 +20,7 @@ router.get('/', async (req, res) => {
 
 //@desc /new (New Books Route)
 router.get('/new', async (req, res) => {
-    try {
-      const authors = await Author.find({})
-      const book = new Book()
-      res.render('books/new', {
-         authors: authors,
-         book: book
-      })
-    } catch (error) {
-      res.redirect('/books')
-    }
+    renderNewPage(res, new Book())
 })
 
 //@desc / (Create Books Route)
@@ -45,12 +36,26 @@ router.post('/', upload.single('cover'), async (req, res) => {
    })
 try{
    const newBook = await book.save()
-   res.redirect('books/${newBook.id}')
-   res.redirect('authors')
+   //res.redirect('books/${newBook.id}')
+   res.redirect('books')
 } catch {
-
+   renderNewPage(res, book, true)
 }
 
 })
+
+async function renderNewPage(res, book, hasError = false) {
+   try {
+      const authors = await Author.find({})
+      const params = {
+         authors: authors,
+         book: book
+      }
+      if(hasError) params.errorMessage = 'Error Creating Book'
+      res.render('books/new', params)
+    } catch {
+      res.redirect('/books')
+    }
+}
 
 module.exports = router
