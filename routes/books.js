656e0984f64135073f9dbfa3473ff1eb0e289bca
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2022-12-26 20:07:37 -0500

    description function added, pagecount, cover image upload, now working on backend. also abandonded organized commenting convention for git commits

--- routes/books.js
+++ routes/books.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const multer = require('multer')
+const path = require('path')
 const Book = require('../models/book')
+const uploadPath = path.join('public', Book.coverImageBasePath)
+const imageMimeTypes = ['images/jpeg', 'images/png', 'images/gifs']
 const Author = require('../models/author')
+const upload = multer({
+   dest: uploadPath,
+   fileFilter: (req, file, callback) => {
+      callback(null, )
+   }
+})
 
 //@desc /books (All Books route)
 router.get('/', async (req, res) => {
@@ -23,8 +33,24 @@ router.get('/new', async (req, res) => {
 })
 
 //@desc / (Create Books Route)
-router.post('/', async (req, res) => {
-   res.send('Create Book')
+router.post('/', upload.single('cover'), async (req, res) => {
+   const fileName = req.file != null ? req.file.filename : null
+   const book = new Book({
+      title: req.body.title,
+      author: req.body.author,
+      publishDate: new Date(req.body.publishDate),
+      pageCount: req.body.pageCount,
+      coverImageName: fileName,
+      description: req.body.description
+   })
+try{
+   const newBook = await book.save()
+   res.redirect('books/${newBook.id}')
+   res.redirect('authors')
+} catch {
+
+}
+
 })
 
 module.exports = router
