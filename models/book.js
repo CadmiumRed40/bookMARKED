9b2fef4f3c18d040abba61104f79a8ada64127ef
Enrique Cabrera <80559746+CadmiumRed40@users.noreply.github.com>
2023-01-03 23:09:11 -0500

    bug when applying buffer to represent cover data in route/books and models/books

--- models/book.js
+++ models/book.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const path = require('path')
+const { stringify } = require('querystring')
 
 const coverImageBasePath = "uploads/bookCovers"
 
@@ -24,7 +25,11 @@ const bookSchema = new mongoose.Schema({
         required: true,
         default: Date.now
     },
-    coverImageName: {
+    coverImage: {
+        type: Buffer,
+        required: true
+    },
+    coverImageType: {
         type: String,
         required: true
     },
@@ -37,8 +42,8 @@ const bookSchema = new mongoose.Schema({
 })
 
 bookSchema.virtual('coverImagePath').get(function(){
-    if(this.coverImageName != null) {
-        return path.join('/', coverImageBasePath, this.coverImageName)
+    if (this.coverImage != null && this.coverImageType != null) {
+        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage}`
     }
 })
 
